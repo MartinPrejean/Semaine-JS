54620544f9092154bc6141868600d3bff8fba37c
MartinPrejean <32875992+MartinPrejean@users.noreply.github.com>
2018-04-12 13:43:02 +0200

    Update Constante.js

--- assets/js/Constante.js
+++ assets/js/Constante.js
@@ -3,4 +3,13 @@ const DIRECTION = {
 	"DROITE" : 2,
 }
 
+let useColors = ['#cc0000', '#66d9eb', '#32c06f']
+let color = useColors[(Math.random()*3)]
+
+function generateColors() {
+  return color
+}
+
+const COLORS = [color]
+
 export default { DIRECTION }
